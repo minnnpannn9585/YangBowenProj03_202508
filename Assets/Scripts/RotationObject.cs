f902b40e853c3a549f90c9ea6d4d80383f08a820
MinPan9585 <panmin677095@gmail.com>
2025-08-29 19:01:38 +0800

    assets finish

---
+++ Assets/Scripts/RotationObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObject : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+    private void Update()
+    {
+        // 旋转
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
